5153bd9be80cab564e7bcfb5964371f10b2eb1da
Yuri Tveritin <kentyku@bk.ru>
2018-01-28 22:16:51 +0400

    Создание front-end  в ClietnUI  и создание фейковых классов REST описывает запросы к веб-сервису JSON подготавливает запросы в формате JSON

---
+++ src/main/java/com/mycompany/mavenprojectclientrestjson/Json.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenprojectclientrestjson;
+
+/**
+ *
+ * @author user
+ * 
+ * формирует строку JSON c тегами: 
+ * /seach
+ * получает
+ * /accepted_answer_id
+ * /карточками авторов постов, авторов ответов
+ */
+public class Json {
+    
+}
