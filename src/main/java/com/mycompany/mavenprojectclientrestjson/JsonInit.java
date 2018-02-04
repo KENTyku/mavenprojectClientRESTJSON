b1431086bc52c35b1efbd1f6fa4fdc8bccc52892
Yuri Tveritin <kentyku@bk.ru>
2018-02-04 23:57:47 +0400

    добавил описание

---
+++ src/main/java/com/mycompany/mavenprojectclientrestjson/JsonInit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenprojectclientrestjson;
+
+/**
+ * 
+ * @author Yuri Tveritin
+ * 
+ *  хранит в отображении  Map пары ключ значение(используется для хранения значений,
+ * передаваемых в строку запроса; для хранения значений, передаваемых 
+ * в залоговок запроса)
+ * 
+ * реализует метод
+ * setStringJSON получает ключ и значение для элемента JSON
+ * getJSON возвращает данные в формате JSON
+ **/
+
+public class JsonInit {
+    //в разработке
+}
